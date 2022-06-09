7196b2a6748f543f0f8413f79d56ab5539f3ad8f
TroyLi <litianstar@gmail.com>
2022-06-09 15:30:37 -0400

    version 0.1 create newsbeing project

---
+++ src/main/java/com/tger/newsbeing/NewsbeingApplication.java
@@ -0,0 +1,13 @@
+package com.tger.newsbeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsbeingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsbeingApplication.class, args);
+    }
+
+}
